7f2940f9fa819a5c15ad85f2612ffdd889683e27
Ajay k <74807473+ra-lgt@users.noreply.github.com>
2023-02-23 16:57:19 +0000

    new_app

--- Social_Media/Home_Page/urls.py
+++ Social_Media/Home_Page/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.home),
+    path('login_signup',views.login_signup),
+    path('register',views.register),
 ]
--- Social_Media/Home_Page/views.py
+++ Social_Media/Home_Page/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+import pyodbc
 
 def home(request):
-    return render(request,'home.html');
+    return render(request,'home.html')
 
+def login_signup(request):
+    return render(request,'login_signup.html')
+
+def register(request):
+    server_name = 'DESKTOP-R3D7KGJ'
+    database_name = 'user_details'
+    username = 'root'
+    password = 'raviajay.2003'
+
+    connection_string = f"Driver={{SQL Server}};Server={server_name};Database={database_name};UID={username};PWD={password}"
+    cnxn = pyodbc.connect(connection_string)
+    print(cnxn)
+    return render(request,'login_signup.html')
